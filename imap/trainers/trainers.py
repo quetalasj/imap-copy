0bbb4a7547bf6ed0a52728fc85fa8441d6fc7e11
Evgeny Kruzhkov <quetalasj@gmail.com>
2022-02-16 18:56:24 +0300

    add slam

--- imap/trainers/trainers.py
+++ imap/trainers/trainers.py
@@ -45,6 +45,90 @@ class ModelTrainer:
             del loss, optimizer
         torch.cuda.empty_cache()
 
+    def slam(self,
+             model,
+             dataset_loader,
+             num_model_epochs,
+             num_poses_epochs,
+             is_image_active_sampling,
+             lr=0.005,
+             verbose=True):
+        """
+        :param model:
+        :param dataset_loader:
+        :param num_model_epochs:
+        :param num_poses_epochs:
+        :param is_image_active_sampling:
+        :param lr:  default 0.005
+        :param verbose:
+        :return:
+        """
+        poses = []
+        with TrainLogger('_slam') as logger:
+            model.cuda()
+            current_position = None
+            zero_state = None
+            states_array = []
+            for state_num, state in enumerate(tqdm(dataset_loader)):
+                if state_num == 0:  # train model
+                    model.requires_grad_(True)
+                    model.train()
+                    state.freeze_position()
+                    state._position.cuda()
+                    zero_state = state
+
+                    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+                    num_epochs = num_model_epochs
+                    states = [zero_state]
+                elif state_num % 10 == 0:   # train model and optimize poses
+                    model.requires_grad_(True)
+                    model.train()
+                    state.set_position(current_position)
+
+                    states_array.append(state)
+                    states = states_array
+
+                    opt_poses = []
+                    for s in states:
+                        s.train_position()
+                        s._position.cuda()
+                        opt_poses.append(s._position)
+
+                    zero_state.freeze_position()
+                    zero_state._position.cuda()
+                    states.append(zero_state)
+
+                    optimizer = torch.optim.Adam([*model.parameters(), *opt_poses], lr=lr)
+                    num_epochs = num_model_epochs
+                else:   # optimize poses
+                    model.eval()
+                    model.requires_grad_(False)
+                    state.set_position(current_position)
+                    state.train_position()
+                    state._position.cuda()
+
+                    optimizer = torch.optim.Adam([state._position])
+                    num_epochs = num_poses_epochs
+                    states = [state]
+
+                for i in trange(num_epochs):
+                    optimizer.zero_grad()
+                    for s in states:
+                        loss, mean_loss = self.backward_model(model, s, is_image_active_sampling)
+                    optimizer.step()
+
+                for s in states:
+                    s.freeze_position()
+                    s._position.cpu()
+                current_position = state.get_matrix_position().detach().numpy()
+                poses.append(current_position.copy())
+
+                logger.log(state, mean_loss, state_num, verbose=verbose)
+                optimizer.zero_grad()
+            del loss, optimizer
+        torch.cuda.empty_cache()
+        return poses
+
     # def localization(self,
     #                  model,
     #                  tracking_dataset_loader,
